94a2427a1dea88c46d124820329822499f3a34b7
atauri00 <87322532+atauri00@users.noreply.github.com>
2021-10-05 19:21:52 +0200

    Add files via upload

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: adiaz-de <adiaz-de@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 15:11:10 by adiaz-de          #+#    #+#             */
-/*   Updated: 2021/10/05 15:26:40 by adiaz-de         ###   ########.Urdlz    */
+/*   Updated: 2021/10/05 19:18:20 by adiaz-de         ###   ########.Urdlz    */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 char *ft_strtrim(char const *s1, char const *set)
 {
-	
+	size_t	len;
+
+	if (!s1 || !set)
+		return (NULL);
+	while (ft_strchr(set, *s1) && *s1)
+		s1++;
+	len = ft_strlen(s1);
+	while (len && ft_strchr(set, s1[len]))
+		len--;
+	return (ft_substr(s1, 0, len + 1));
 }
